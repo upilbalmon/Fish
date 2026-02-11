de15a17946f507e24f69522cd489ac0ac79bce32
upilbalmon <balmonupil@gmail.com>
2026-02-11 21:30:12 +0700

    Update AllSC.lua

--- AllSC.lua
+++ AllSC.lua
@@ -7,10 +7,8 @@ local scriptURLs = {
     "https://raw.githubusercontent.com/4LynxX/Lynx/refs/heads/main/NewGuiLynx.lua",
     "https://raw.githubusercontent.com/MajestySkie/Chloe-X/main/Main/ChloeX",
     "https://api.luarmor.net/files/v3/loaders/9eec60d0a187310c898cbfe27fb1c18b.lua",
-
-"https://api.luarmor.net/files/v3/loaders/aea30adec1896c44e417cd42ff3b11c3.lua",
-
-"https://raw.githubusercontent.com/thantzy/thanhub/refs/heads/main/thanv1"
+    "https://api.luarmor.net/files/v3/loaders/aea30adec1896c44e417cd42ff3b11c3.lua",
+    "https://raw.githubusercontent.com/thantzy/thanhub/refs/heads/main/thanv1"
 }
 
 local buttonNames = {"LynX", "ChloeX", "TrixHub", " Voltra", "ThanV1" }
